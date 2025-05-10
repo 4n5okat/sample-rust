996b6d8736c21aed56c1767948f71ff613b8d5b5
4n5okat <pub.shingo.tako@gmail.com>
2025-05-10 13:11:50 +0900

    edit

--- app/src/errors/mod.rs
+++ app/src/errors/mod.rs
@@ -1,9 +1,1 @@
 // src/errors/mod.rs
-
-// サインアップに関連するエラーハンドリングを定義したモジュール
-pub mod sign_up;
-
-// メールアドレス確認に関連するエラーハンドリングを定義したモジュール
-pub mod verify_email;
-
-pub mod sign_in;
--- app/src/routes/mod.rs
+++ app/src/routes/mod.rs
@@ -1,1 +1,8 @@
 // src/routes/mod.rs
+
+// Actix Web の web モジュールをインポート（ルーティング設定に使用）
+use actix_web::web;
+
+pub fn init_routes(cfg: &mut web::ServiceConfig) {
+
+}
